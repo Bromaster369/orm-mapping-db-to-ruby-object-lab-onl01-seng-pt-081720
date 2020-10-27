fd260062dc9bd071145eb13372da0c3438acfd31
Christian Pardo <chrispardopersonal@gmail.com>
2020-10-27 00:30:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,7 +79,14 @@ class Student
       end.first
   end
   
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ? 
+      
+    
+    SQL
     
     
   end 
